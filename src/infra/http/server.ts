c06a40399c2a95781662410c92a91c6105915803
Deibson Cogo <deibsoncogo@outlook.com>
2025-01-17 20:40:39 -0300

    feat: criação da validação e serialização de dados

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,7 +1,11 @@
 import { fastifyCors } from "@fastify/cors"
 import { fastify } from "fastify"
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
 
 const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
 
 server.register(fastifyCors, { origin: "*" })
 
