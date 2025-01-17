e1c769e7921cbbcebdc2dc8950d4a223586cb4d4
Deibson Cogo <deibsoncogo@outlook.com>
2025-01-17 20:41:27 -0300

    feat: criação da tratativa de erros

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,11 +1,28 @@
 import { fastifyCors } from "@fastify/cors"
 import { fastify } from "fastify"
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod"
 
 const server = fastify()
 
 server.setValidatorCompiler(validatorCompiler)
 server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: "Validation error",
+      issues: error.validation,
+    })
+  }
+
+  console.error("Error =>", error)
+
+  return reply.status(500).send({ message: "Internal server error" })
+})
 
 server.register(fastifyCors, { origin: "*" })
 
